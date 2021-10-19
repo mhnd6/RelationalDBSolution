e99c53308129f25c85327af97e4de9b54482b1e6
Mhnd <mrmohandtv@gmail.com>
2021-10-20 02:52:17 +0300

    Integration with Sqlite

---
+++ DataAccessLibrary/SqliteDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Data.SQLite;
+using Dapper;
+using System.Linq;
+
+namespace DataAccessLibrary
+{
+    public class SqliteDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatment, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatment, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatment, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatment, parameters);
+            }
+        }
+    }
+}
---
+++ SQLiteUI/Program.cs
@@ -0,0 +1,95 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SQLiteUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqliteCRUD sql = new SqliteCRUD(GetConnectionString());
+
+            ReadAllContacts(sql);
+
+            //ReadContact(sql, 2);
+
+            CreateNewContact(sql);
+
+            //UpdateContact(sql);
+
+            //RemovePhoneNumberFromContact(sql, 1, 1);
+
+            Console.WriteLine("Done proccessing sqlite");
+            Console.ReadLine();
+        }
+
+        private static void RemovePhoneNumberFromContact(SqliteCRUD sql, int contactId, int phoneNumberId)
+        {
+            sql.RemovePhoneNumberFromContact(contactId, phoneNumberId);
+        }
+        private static void UpdateContact(SqliteCRUD sql)
+        {
+            BasicContactModel contact = new BasicContactModel
+            {
+                Id = 1,
+                FirstName = "Rabea",
+                LastName = "Abdulgaffar"
+            };
+
+            sql.UpdateContactName(contact);
+        }
+        private static void CreateNewContact(SqliteCRUD sql)
+        {
+            FullContactModel user = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "Amjad",
+                    LastName = "Bukhari"
+                }
+            };
+
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "kamal2Alahli.com" });
+
+            user.PhoneNumbers.Add(new PhoneNumberModel { Id = 1, PhoneNumber = "554537104" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "39875645" });
+
+            sql.CreateContact(user);
+
+        }
+        private static void ReadAllContacts(SqliteCRUD sql)
+        {
+            var rows = sql.GetAllContacts();
+
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id}: { row.FirstName } { row.LastName }");
+            }
+        }
+
+        private static void ReadContact(SqliteCRUD sql, int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+
+            Console.WriteLine($"{contact.BasicInfo.Id}: { contact.BasicInfo.FirstName } { contact.BasicInfo.LastName }");
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
+}
